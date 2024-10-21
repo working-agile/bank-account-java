2ed4b9a1f998b1a5eca1fb593c8955bf4637ae83
Nathan <nathansilva009@gmail.com>
2024-10-21 15:10:37 -0300

    Creating bank account with tdd

--- src/main/java/com/workingagile/acsd/BankAccount.java
+++ src/main/java/com/workingagile/acsd/BankAccount.java
@@ -1,4 +1,39 @@
 package com.workingagile.acsd;
 
 public class BankAccount {
+
+    private int balance;
+    private final int transferenceFee;
+
+    public BankAccount(int initialBalance) {
+        this.balance = initialBalance;
+        this.transferenceFee = 0;
+    }
+
+    public BankAccount(int initialBalance, int transferenceFee) {
+        this.balance = initialBalance;
+        this.transferenceFee = transferenceFee;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance = balance + amount;
+    }
+
+    public void withdraw(int amount) throws InsufficientBalanceException {
+        if (amount > balance) {
+            throw new InsufficientBalanceException();
+        }
+        balance = balance - amount;
+    }
+
+    public void transfer(int amount, BankAccount receiverAccount) throws InsufficientBalanceException {
+        withdraw(amount + transferenceFee);
+        receiverAccount.deposit(amount);
+    }
+
+    public static class InsufficientBalanceException extends Exception {}
 }
--- src/test/java/com/workingagile/acsd/BankAccountUnitTest.java
+++ src/test/java/com/workingagile/acsd/BankAccountUnitTest.java
@@ -1,4 +1,103 @@
 package com.workingagile.acsd;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BankAccountUnitTest {
+
+    @DisplayName("Deposits should increase the balance according to the amount")
+    @Test
+    void depositAmount() {
+        // Arrange (Given)
+        BankAccount bankAccount = new BankAccount(1000);
+
+        // Act (When)
+        bankAccount.deposit(200);
+
+        // Assert (Then)
+        assertThat(bankAccount.getBalance(), is(equalTo(1200)));
+    }
+
+    @DisplayName("Withdrawals should decrease the balance according to the amount")
+    @Test
+    void withdrawAmount() throws Exception {
+        // Arrange (Given)
+        BankAccount bankAccount = new BankAccount(1000);
+
+        // Act (When)
+        bankAccount.withdraw(200);
+
+        // Assert (Then)
+        assertThat(bankAccount.getBalance(), is(equalTo(800)));
+    }
+
+    @DisplayName("Overdrawing the bank account is not allowed")
+    @Test
+    void overdrawingAmount() {
+        // Arrange (Given)
+        BankAccount bankAccount = new BankAccount(1000);
+
+        // Act (When)
+        try {
+            bankAccount.withdraw(1100);
+            fail("withdrawal not expected to go through");
+        } catch (BankAccount.InsufficientBalanceException e) {}
+
+        // Assert (Then)
+        assertThat(bankAccount.getBalance(), is(equalTo(1000)));
+    }
+
+    @DisplayName("Should transfer a value to other account")
+    @Test
+    void shouldTransferMoneyToOtherBankAccount() throws Exception {
+        // Arrange (Given)
+        BankAccount bankAccount1 = new BankAccount(1000);
+        BankAccount bankAccount2 = new BankAccount(0);
+
+        // Act (When)
+        bankAccount1.transfer(500, bankAccount2);
+
+        // Assert (Then)
+        assertThat(bankAccount1.getBalance(), is(equalTo(500)));
+        assertThat(bankAccount2.getBalance(), is(equalTo(500)));
+    }
+
+    @DisplayName("Transfer amount higher than the balance, should not transfer")
+    @Test
+    void shouldNotTransferWhenTransferAmountIsHigherThanTheBalance() {
+        // Arrange (Given)
+        BankAccount bankAccount1 = new BankAccount(1000);
+        BankAccount bankAccount2 = new BankAccount(0);
+
+        // Act (When)
+        try {
+            bankAccount1.transfer(1100, bankAccount2);
+            fail("transfer not expected to go through");
+        } catch (BankAccount.InsufficientBalanceException e) {}
+
+        // Assert (Then)
+        assertThat(bankAccount1.getBalance(), is(equalTo(1000)));
+        assertThat(bankAccount2.getBalance(), is(equalTo(0)));
+    }
+
+    @DisplayName("Transference fee is charged when transferring money")
+    @Test
+    void shouldApplyTransferenceFeeWhenTransferringToOtherBankAccount() throws Exception {
+        // Arrange (Given)
+        int transferenceFee = 10;
+        BankAccount bankAccount1 = new BankAccount(1000, transferenceFee);
+        BankAccount bankAccount2 = new BankAccount(0, transferenceFee);
+
+        // Act (When)
+        bankAccount1.transfer(500, bankAccount2);
+
+        // Assert (Then)
+        assertThat(bankAccount1.getBalance(), is(equalTo(490)));
+        assertThat(bankAccount2.getBalance(), is(equalTo(500)));
+    }
 }
