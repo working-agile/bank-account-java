85be53fd9798cd4eef5cc5674b6d70184ef39263
Axel <axelberle@yahoo.de>
2024-10-23 13:07:09 -0300

    initial unit test

--- src/test/java/com/workingagile/acsd/BankAccountUnitTest.java
+++ src/test/java/com/workingagile/acsd/BankAccountUnitTest.java
@@ -1,4 +1,21 @@
 package com.workingagile.acsd;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BankAccountUnitTest {
+
+    @DisplayName("A unit test...")
+    @Test
+    void a_unit_test() {
+        // Arrange (Given)
+        System.out.println("Arrange something...");
+
+        // Act (When)
+        System.out.println("Execute something...");
+
+        // Assert (Then)
+        System.out.println("Assert something...");
+
+    }
 }
