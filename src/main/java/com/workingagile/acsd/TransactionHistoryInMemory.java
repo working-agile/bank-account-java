170ff64e1aa545fa819952f35e2b9b0bc824a9ca
Axel <axelberle@yahoo.de>
2024-10-24 16:01:27 -0300

    first 2 baby steps

--- src/main/java/com/workingagile/acsd/TransactionHistoryInMemory.java
+++ src/main/java/com/workingagile/acsd/TransactionHistoryInMemory.java
@@ -1,15 +1,30 @@
 package com.workingagile.acsd;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionHistoryInMemory implements TransactionHistory{
 
+    private List<Integer> transactions;
+
+    public TransactionHistoryInMemory() {
+        transactions = new ArrayList<>();
+    }
+
     @Override
     public void informTransaction(String transaction, int amount) {
+        if (transaction.equals("withdraw")) {
+            amount = -amount;
+        }
+        transactions.add(amount);
     }
 
     @Override
     public List<Integer> getTransactionHistory() {
-        return null;
+        return transactions;
     }
+
+
+
+
 }
--- src/test/java/com/workingagile/acsd/TransactionHistoryInMemoryTest.java
+++ src/test/java/com/workingagile/acsd/TransactionHistoryInMemoryTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
 
 public class TransactionHistoryInMemoryTest {
 
@@ -25,6 +26,41 @@ public class TransactionHistoryInMemoryTest {
         assertThat(transactionList, is(empty()));
     }
 
+    @DisplayName("informing a single deposit")
+    @Test
+    void informing_a_single_deposit() {
+
+        // Arrange (Given)
+        TransactionHistoryInMemory transactionHistory = new TransactionHistoryInMemory();
+
+        // Act (When)
+        transactionHistory.informTransaction("deposit", 50);
+
+        // Assert (Then)
+        List<Integer> transactionList = transactionHistory.getTransactionHistory();
+        assertThat(transactionList, is(not(empty())));
+        assertThat(transactionList.size(), is(1));
+        assertThat(transactionList.get(0), is(50));
+
+    }
+
+    @DisplayName("informing a single withdrawal")
+    @Test
+    void informing_a_single_withdrawal() {
+
+        // Arrange (Given)
+        TransactionHistoryInMemory transactionHistory = new TransactionHistoryInMemory();
+
+        // Act (When)
+        transactionHistory.informTransaction("withdraw", 50);
+
+        // Assert (Then)
+        List<Integer> transactionList = transactionHistory.getTransactionHistory();
+        assertThat(transactionList, is(not(empty())));
+        assertThat(transactionList.size(), is(1));
+        assertThat(transactionList.get(0), is(-50));
+
+    }
 
 
 }
