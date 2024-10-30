e3d714d88072b8e0dbb9bf33d1d65cc6a69aa6c0
Axel <axelberle@yahoo.de>
2024-10-30 09:24:23 -0300

    implemented first banking scenario

--- src/test/java/com/workingagile/acsd/bdd/bankaccount/BankAccountStepDefs.java
+++ src/test/java/com/workingagile/acsd/bdd/bankaccount/BankAccountStepDefs.java
@@ -1,5 +1,8 @@
 package com.workingagile.acsd.bdd.bankaccount;
 
+import com.workingagile.acsd.BankAccount;
+import com.workingagile.acsd.EmailSender;
+import com.workingagile.acsd.TransactionHistory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,26 +10,35 @@ import io.cucumber.java.en.When;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class BankAccountStepDefs {
 
+    BankAccount myBankAccount;
+
     @Given("a bank account with the initial balance of {int}")
-    public void a_bank_account_with_the_initial_balance_of(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void a_bank_account_with_the_initial_balance_of(int initialBalance)  {
+
+        EmailSender emailSender = mock(EmailSender.class);
+        TransactionHistory transactionHistory = mock(TransactionHistory.class);
+
+        myBankAccount = new BankAccount(initialBalance, 0, emailSender,
+                transactionHistory);
+
     }
 
     @When("a client deposits {int}")
-    public void a_client_deposits(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void a_client_deposits(int amountToDeposit) {
+
+        myBankAccount.deposit(amountToDeposit);
+
     }
 
     @Then("the account should have a balance of {int}")
-    public void the_account_should_have_a_balance_of(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    public void the_account_should_have_a_balance_of(int expectedBalance) {
 
+        int currentBalance = myBankAccount.getBalance();
+        assertThat(currentBalance, is(equalTo(expectedBalance)));
+    }
 
 }
