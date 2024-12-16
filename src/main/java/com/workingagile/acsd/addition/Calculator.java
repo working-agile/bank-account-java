b70201a9bc1e9d222260fa20a3bb60c4de542ee7
Axel <axelberle@yahoo.de>
2024-12-16 15:27:26 -0300

    failing scenario

---
+++ src/main/java/com/workingagile/acsd/addition/Calculator.java
@@ -0,0 +1,10 @@
+package com.workingagile.acsd.addition;
+
+public class Calculator {
+    public void add(Integer addend1, Integer addend2) {
+    }
+
+    public Integer getCurrentValue() {
+        return null;
+    }
+}
--- src/test/java/com/workingagile/acsd/addition/AdditionStepDefs.java
+++ src/test/java/com/workingagile/acsd/addition/AdditionStepDefs.java
@@ -13,23 +13,29 @@ public class AdditionStepDefs {
     int addend1;
     int addend2;
 
+    Calculator calculator;
+
     @Given("two addends {int} and {int}")
     public void two_addends_and(Integer addend1, Integer addend2) {
         this.addend1 = addend1;
         this.addend2 = addend2;
     }
 
-    int sum;
-
     @When("they are added")
     public void they_are_added() {
 
-        sum = addend1 + addend2;
+        calculator = new Calculator();
+        calculator.add(addend1, addend2);
+
     }
 
     @Then("the sum should be {int}")
     public void the_sum_should_be(Integer expectedSum) {
-        assertThat(sum, is(equalTo(expectedSum)));
+
+        Integer actualSum = calculator.getCurrentValue();
+
+        assertThat(actualSum, is(equalTo(expectedSum)));
+
     }
 
 }
