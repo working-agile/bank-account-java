e2d1e27cec52d76fcbdabb1fac03cd0c7233ab37
Axel <axelberle@yahoo.de>
2024-12-16 15:30:02 -0300

    passing scenario

--- src/main/java/com/workingagile/acsd/addition/Calculator.java
+++ src/main/java/com/workingagile/acsd/addition/Calculator.java
@@ -1,10 +1,14 @@
 package com.workingagile.acsd.addition;
 
 public class Calculator {
+
+    private int currentValue;
+
     public void add(Integer addend1, Integer addend2) {
+        this.currentValue = addend1 + addend2;
     }
 
     public Integer getCurrentValue() {
-        return null;
+        return currentValue;
     }
 }
