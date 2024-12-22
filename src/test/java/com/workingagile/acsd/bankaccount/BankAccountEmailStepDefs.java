2d7d7506192b645b769b5be585203f181f5ffc70
Axel <axelberle@yahoo.de>
2024-12-22 17:10:51 -0300

    failing test

--- src/test/java/com/workingagile/acsd/bankaccount/BankAccountEmailStepDefs.java
+++ src/test/java/com/workingagile/acsd/bankaccount/BankAccountEmailStepDefs.java
@@ -4,16 +4,22 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
 
 public class BankAccountEmailStepDefs {
 
+
+    EmailSender mockEmailSender;
+
     @When("a client overdrafts his account")
     public void a_client_overdrafts_his_account() {
 
+        mockEmailSender = mock(EmailSender.class);
+
         try {
 
-            BankAccount bankAccount = new BankAccount(1000);
+            BankAccount bankAccount = new BankAccount(1000, mockEmailSender);
             bankAccount.withdraw(2000);
             fail("Not expected to get here");
 
@@ -25,8 +31,8 @@ public class BankAccountEmailStepDefs {
     @Then("an email alert is sent to the bank")
     public void an_email_alert_is_sent_to_the_bank() {
 
-        // TODO: verify that the email has been sent
-        fail("Email has not been sent");
+        verify(mockEmailSender, times(1)).sendEmailToBank();
+
     }
 
 }
