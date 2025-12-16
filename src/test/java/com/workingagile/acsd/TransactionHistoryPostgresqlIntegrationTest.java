ac79373960d1c7028e21c2ea643e98eb2ac64662
Axel Wilhelm Berle <axelberle@yahoo.de>
2025-12-16 13:45:33 -0300

    rename TestContainerTests to TransactionHistoryPostgresqlIntegrationTest

--- src/test/java/com/workingagile/acsd/TransactionHistoryPostgresqlIntegrationTest.java
+++ src/test/java/com/workingagile/acsd/TransactionHistoryPostgresqlIntegrationTest.java
@@ -16,7 +16,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 
-public class TestContainerTests {
+public class TransactionHistoryPostgresqlIntegrationTest {
 
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
             "postgres:16-alpine"
